33e58240eecfd33d0db30693583b6502f65e84d7
Tim Tijssens <timtijssens@gmail.com>
2015-07-29 14:55:27 +0200

    Added routes that return serviceupdates and tripupdates in JSON

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
 
 var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 var request = require('request');
+var express = require('express');
+var app = express();
 
 var requestSettingsTripUpdates = {
     method: 'GET',
@@ -34,20 +36,50 @@ request(requestSettingsTripUpdates, function (error, response, body) {
     }
 });
 */
-request(requestSettingsServiceAlerts, function (error, response, body) {
-
-    if (!error && response.statusCode == 200) {
-
-        var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
 
 
-        //console.log(feed.entity[0]);
-        feed.entity.forEach(function(entity) {
 
-            console.log(entity.alert.header_text.translation[0]);
-            if (entity.trip_update) {
-                console.log(entity.trip_update);
-            }
-        });
-    }
+
+app.get('/servicesAlerts', function (req, res) {
+
+    request(requestSettingsServiceAlerts, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+
+            var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+
+
+            //console.log(feed.entity[0]);
+            feed.entity.forEach(function(entity) {
+                 res.write(JSON.stringify(entity));
+                console.log(entity.alert.header_text.translation[0]);
+
+            });
+        }
+    });
+});
+
+app.get('/tripUpdates', function (req, res) {
+
+    request(requestSettingsTripUpdates, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+
+            var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+
+            feed.entity.forEach(function(entity) {
+                res.write(JSON.stringify(entity));
+                if (entity.trip_update) {
+                    console.log(entity.trip_update);
+                }
+            });
+        }
+    });
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
 });
